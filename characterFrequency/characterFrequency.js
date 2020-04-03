436e20ac62af5072dbd42f5fe4e2856e5bd1aa89
Jackson Reinagel <jreinagel4@gmail.com>
2020-04-03 11:05:44 -0600

    Update with completed characterFrequency function

--- characterFrequency/characterFrequency.js
+++ characterFrequency/characterFrequency.js
@@ -35,7 +35,16 @@
  *
  */
 
-
 var characterFrequency = function(string) {
-  return result;
+  let charFreq = {};
+  for (let i = 0; i < string.length; i++) {
+  	let currChar = string[i];
+  	charFreq[currChar] === undefined ? charFreq[currChar] = 1 : charFreq[currChar]++;
+  }
+  let value = charFreq[1];
+  console.log(charFreq[1]);
+  let descending = Object.entries(charFreq).sort(function(a,b) {
+    return b[1] > a[1] ? true : (a[0] > b[0] ? true : false);
+  });
+  return descending;
 };
