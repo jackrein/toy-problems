831cf653b6cd159557d825fbb796d092ffd6678a
Jackson Reinagel <jreinagel4@gmail.com>
2020-04-20 10:32:58 -0600

    Update with completed fractionConverter and sudokuChecker

--- fractionConverter/fractionConverter.js
+++ fractionConverter/fractionConverter.js
@@ -1,6 +1,5 @@
 /**
- * Write a function that takes a number as its argument and 
- * returns a string that represents that number's simplified fraction.
+ * Write a function that takes a number as its argument and returns a string that represents that number's simplified fraction.
  *
  * Example: toFraction(0.5) === '1/2'
  * 
@@ -13,5 +12,21 @@
  */
 
 var toFraction = function(number) {
-  // Your code here
+  let denom = 1;
+  if (number < 0) {
+    return '-' + toFraction(-number);
+  } else {
+    while (!Number.isInteger(number)) {
+      number *= 10;
+      denom *= 10;
+    }
+  }
+  let gcd = 1;
+  for (let d = number; d > 0; d--) {
+    if (number % d === 0 && denom % d === 0) {
+      gcd = d;
+      break;
+    }
+  }
+  return number/gcd + '/' + denom/gcd;
 };
--- sudokuChecker/sudokuChecker.js
+++ sudokuChecker/sudokuChecker.js
@@ -18,9 +18,37 @@ Example input:
 157638429"
 */
 
-
-
 function sudokuChecker(board) {
-  // Your code here.
-  
-}
+  let matrix = board.split('\n ').map(function(row) {
+    return row.split('').map(function(digit) {
+      return parseInt(digit);
+    });
+  });
+  function invalid(arr) {
+    return arr.length !== 9 ||
+    arr.reduce(function(a,b) { return a+b }, 0) !== 45 ||
+    arr.indexOf('5') !== arr.lastIndexOf('5');
+  }
+  for (let row = 0; row < 9; row++) {
+    if (invalid(matrix[row])) {
+      return 'invalid row: ' + row;
+    }
+  }
+  for (let col = 0; col < 9; col++) {
+    if (invalid(matrix.map(function(row) {
+      return row[col];
+    }))) return 'invalid col: ' + col;
+  }
+  for (let col = 0; col < 9; col+=3) {
+    for (let row = 0; row < 9; row+=3) {
+      let row1 = matrix[row].splice(0,3);
+      let row2 = matrix[row+1].splice(0,3);
+      let row3 = matrix[row+2].splice(0,3);
+      if (invalid(row1.concat(row2, row3))) {
+        return 'invalid';
+      }
+    }
+  } return 'solved';
+};
+
+sudokuChecker(`735814296\n 896275314\n 214963857\n 589427163\n 362189745\n 471356982\n 923541678\n 648792531\n 157638429`);
